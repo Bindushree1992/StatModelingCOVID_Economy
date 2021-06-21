d8d50fb8c9c7581b950a6c298b46098a153bcd39
Bindushree1992 <48619269+Bindushree1992@users.noreply.github.com>
2021-06-21 21:55:19 +0200

    select tickers
    List stock market tickers for 6 countries if possible.

---
+++ tickers.py
@@ -0,0 +1,8 @@
+us_tickers = [
+    'AAPL', 'MSFT', 'AMZN', 'GOOG', 'GOOGL', 'FB', 'TSLA', 'BABA', 'TSM',
+    'BRK-A', 'BRK-B', 'V', 'JNJ', 'WMT', 'JPM', 'PG', 'MA', 'UNH', 'DIS',
+    'NVDA', 'HD', 'BAC', 'PYPL', 'VZ', 'CMCSA', 'ADBE', 'NKE', 'NFLX', 'KO',
+    'TM', 'T', 'INTC', 'ASML', 'NVS', 'MRK', 'PFE', 'CRM', 'PEP', 'ABT', 'TMO',
+    'XOM', 'CSCO', 'ABBV', 'ORCL', 'CVX', 'ACN', 'AVGO', 'QCOM', 'DHR', 'QCOM'
+]
+
